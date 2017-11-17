1997792f276f17b94804bb13a8513f1591934491
Katherine Over <katherine.over@gmail.com>
2017-11-17 09:55:55 -0500

    add routing: complete

---
+++ src/components/Landing.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Landing = () => (
+  <section className="landing">
+  </section>
+);
+
+export default Landing;
---
+++ src/components/Library.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Library = () => (
+  <section className="libary">
+  </section>
+);
+
+export default Library;
