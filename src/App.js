64e9993f0dfa0ca2d5d1a76177ad7b8385ce2520
Katherine Over <katherine.over@gmail.com>
2017-12-04 13:51:59 -0500

    add react bootstrap

--- src/App.js
+++ src/App.js
@@ -4,24 +4,31 @@ import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header>
-          <nav>
-            <Link to='/'>Landing</Link>
-            <Link to='/library'>Library</Link>
-          </nav>
-          <h1>Bloc Jams</h1>
-        </header>
-        <main>
-          <Route exact path="/" component={Landing} />
-          <Route path="/library" component={Library} />
-          <Route path="/album/:slug" component={Album} />
-        </main>
-      </div>
+      <Grid fluid>
+        <Row className="show-grid">
+          <Col md={12}>
+            <header>
+              <nav>
+                <Link to='/'>Landing</Link>
+                <Link to='/library'>Library</Link>
+              </nav>
+              <h1>Bloc Jams</h1>
+            </header>
+          </Col>
+          <Col md={12}>
+            <main>
+              <Route exact path="/" component={Landing} />
+              <Route path="/library" component={Library} />
+              <Route path="/album/:slug" component={Album} />
+            </main>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
