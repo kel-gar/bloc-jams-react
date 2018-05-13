c5a21733dad3f81dd0c49846bd075d56935e0a53
Kelly Garry <kellygarry@KG-Macbook-Air.local>
2018-05-12 22:10:04 -0600

    Checkpoint-8-PlayerBarButtons-Assignment:Completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -70,6 +70,14 @@ class Album extends Component {
       this.play();
     }
 
+    handleNextClick() {
+      const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+      const newIndex = Math.min(this.state.album.songs.length - 1, currentIndex + 1);
+      const newSong = this.state.album.songs[newIndex];
+      this.setSong(newSong);
+      this.play();
+    }
+
    render() {
      return (
        <section className="album">
@@ -112,6 +120,7 @@ class Album extends Component {
            currentSong={this.state.currentSong}
            handleSongClick={() => this.handleSongClick(this.state.currentSong)}
            handlePrevClick={() => this.handlePrevClick()}
+           handleNextClick={() => this.handleNextClick()}
          />
        </section>
      );
